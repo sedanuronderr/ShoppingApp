4e6a0fdbde6ef9afc8a052f1dcc0684f699834ae
SedaNur35 <sednrondr@gmail.com>
2022-03-04 23:44:23 +0300

    commit

--- app/src/main/java/com/seda/shoppingapp/activies/UserProfilFragment.kt
+++ app/src/main/java/com/seda/shoppingapp/activies/UserProfilFragment.kt
@@ -1,7 +1,12 @@
 package com.seda.shoppingapp.activies
 
+import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.text.TextUtils
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -15,6 +20,8 @@ import com.seda.shoppingapp.Firestore.FirestoreClass
 import com.seda.shoppingapp.R
 import com.seda.shoppingapp.databinding.FragmentUserProfilBinding
 import com.seda.shoppingapp.model.User
+import com.seda.shoppingapp.utils.GlideLoader
+import java.io.IOException
 import java.util.jar.Manifest
 
 
@@ -65,11 +72,10 @@ class UserProfilFragment : Fragment(),View.OnClickListener {
                 R.id.user_photo ->{
                     if(ContextCompat.checkSelfPermission(requireContext(),android.Manifest.permission.READ_EXTERNAL_STORAGE)
                         == PackageManager.PERMISSION_GRANTED){
-                        view?.let {
-                            FirestoreClass.showErrorSnackBar("You already have the storage","",
-                                it
-                            )
-                        }
+
+                            val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                        startActivityForResult(galleryIntent,1)
+
                     }
                     else{
                         ActivityCompat.requestPermissions(requireActivity(), arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
@@ -88,11 +94,43 @@ class UserProfilFragment : Fragment(),View.OnClickListener {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if(requestCode == 2){
             if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                view?.let { FirestoreClass.showErrorSnackBar("The store Permission","", it) }
+
+                val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(galleryIntent,1)
             }
             else{
                 Toast.makeText(requireContext(),"sorun",Toast.LENGTH_LONG).show()
             }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+if(resultCode == Activity.RESULT_OK){
+    if(data != null){
+        try{
+            val selected = data.data!!
+            //   binding.userPhoto.setImageURI(Uri.parse(selected.toString()))
+
+            GlideLoader(requireContext()).loadUserPicture(selected,binding.userPhoto)
+        }catch (e:IOException){
+            e.printStackTrace()
+        }
+    }
+    else if(resultCode == Activity.RESULT_CANCELED){
+        Log.e("request canceled","ımage")
+    }
+}
+
+    }
+    private fun validateUserProfileDetails():Boolean{
+        return when{
+            TextUtils.isEmpty(binding.mobilNumber.text.toString().trim())->{
+             false
+            }
+         else ->{
+             true
+         }
         }
     }
 
---
+++ app/src/main/java/com/seda/shoppingapp/utils/GlideLoader.kt
@@ -0,0 +1,27 @@
+package com.seda.shoppingapp.utils
+
+import android.content.Context
+import android.net.Uri
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.seda.shoppingapp.R
+import java.io.IOException
+import java.net.URI
+
+class GlideLoader(val context: Context) {
+
+    fun loadUserPicture(imageURI: Uri, imageView: ImageView){
+         try {
+             Glide
+                 .with(context)
+                 .load(Uri.parse(imageURI.toString()))
+                 .centerCrop()
+                 .placeholder(R.drawable.user_background)
+                 .into(imageView)
+         }catch (e:IOException){
+             e.printStackTrace()
+         }
+    }
+
+
+}
